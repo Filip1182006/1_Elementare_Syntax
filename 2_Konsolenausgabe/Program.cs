b31ee36b101a52f64f2017f51e46679e685702d0
tranc <tranc@LAPTOP-I09VUH7L>
2024-10-10 17:14:04 +0200

    Projektdateien hinzufügen.

---
+++ 2_Konsolenausgabe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Konsolenausgabe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fubar");
+        }
+    }
+}

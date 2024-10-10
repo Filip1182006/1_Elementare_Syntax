5fdd5a34cdce53dd433a116433a571641670db76
Filip Bednaruk <filip.b3@gso.schule.koeln>
2024-10-10 17:17:57 +0200

    Deutsche Übersetzung hinzugefügt.

--- GrundlagenrepositoryCsharp/Program.cs
+++ GrundlagenrepositoryCsharp/Program.cs
@@ -10,6 +10,12 @@ namespace GrundlagenrepositoryCsharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hallo Welt");
+
+
+
+
         }
     }
 }
